7ab3151e81e679dce369fb7e78e65037bc4b0d5a
etheral12138 <1694815257@qq.com>
2023-02-03 03:33:18 +0800

    修改于2-2

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import {defineUserConfig} from "vuepress";
 import theme from "./theme";
 import {componentsPlugin} from "vuepress-plugin-components";
+import {docsearchPlugin} from "@vuepress/plugin-docsearch";
 // import {docsearchPlugin} from "@vuepress/plugin-docsearch";
 export default defineUserConfig({
     base: "/",
@@ -24,54 +25,55 @@ export default defineUserConfig({
                 }
             },
         ),
-        // docsearchPlugin({
-        //     apiKey: "7cfd751d4c7b5fe638591fc73c174a7d",
-        //     appId: "4HTLIER5EQ",
-        //     indexName: "etheral",
-        //     locales: {
-        //         "/": {
-        //             placeholder: "搜索文档",
-        //             translations: {
-        //                 button: {
-        //                     buttonText: "搜索文档",
-        //                     buttonAriaLabel: "搜索文档",
-        //                 },
-        //                 modal: {
-        //                     searchBox: {
-        //                         resetButtonTitle: "清除查询条件",
-        //                         resetButtonAriaLabel: "清除查询条件",
-        //                         cancelButtonText: "取消",
-        //                         cancelButtonAriaLabel: "取消",
-        //                     },
-        //                     startScreen: {
-        //                         recentSearchesTitle: "搜索历史",
-        //                         noRecentSearchesText: "没有搜索历史",
-        //                         saveRecentSearchButtonTitle: "保存至搜索历史",
-        //                         removeRecentSearchButtonTitle: "从搜索历史中移除",
-        //                         favoriteSearchesTitle: "收藏",
-        //                         removeFavoriteSearchButtonTitle: "从收藏中移除",
-        //                     },
-        //                     errorScreen: {
-        //                         titleText: "无法获取结果",
-        //                         helpText: "你可能需要检查你的网络连接",
-        //                     },
-        //                     footer: {
-        //                         selectText: "选择",
-        //                         navigateText: "切换",
-        //                         closeText: "关闭",
-        //                         searchByText: "搜索提供者",
-        //                     },
-        //                     noResultsScreen: {
-        //                         noResultsText: "无法找到相关结果",
-        //                         suggestedQueryText: "你可以尝试查询",
-        //                         openIssueText: "你认为该查询应该有结果？",
-        //                         openIssueLinkText: "点击反馈",
-        //                     },
-        //                 },
-        //             },
-        //         },
-        //     },
-        // }),
+        docsearchPlugin({
+            apiKey: "7cfd751d4c7b5fe638591fc73c174a7d",
+            appId: "4HTLIER5EQ",
+            indexName: "etheral",
+            disableUserPersonalization: true,
+            locales: {
+                "/": {
+                    placeholder: "搜索文档",
+                    translations: {
+                        button: {
+                            buttonText: "搜索文档",
+                            buttonAriaLabel: "搜索文档",
+                        },
+                        modal: {
+                            searchBox: {
+                                resetButtonTitle: "清除查询条件",
+                                resetButtonAriaLabel: "清除查询条件",
+                                cancelButtonText: "取消",
+                                cancelButtonAriaLabel: "取消",
+                            },
+                            startScreen: {
+                                recentSearchesTitle: "搜索历史",
+                                noRecentSearchesText: "没有搜索历史",
+                                saveRecentSearchButtonTitle: "保存至搜索历史",
+                                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                                favoriteSearchesTitle: "收藏",
+                                removeFavoriteSearchButtonTitle: "从收藏中移除",
+                            },
+                            errorScreen: {
+                                titleText: "无法获取结果",
+                                helpText: "你可能需要检查你的网络连接",
+                            },
+                            footer: {
+                                selectText: "选择",
+                                navigateText: "切换",
+                                closeText: "关闭",
+                                searchByText: "搜索提供者",
+                            },
+                            noResultsScreen: {
+                                noResultsText: "无法找到相关结果",
+                                suggestedQueryText: "你可以尝试查询",
+                                openIssueText: "你认为该查询应该有结果？",
+                                openIssueLinkText: "点击反馈",
+                            },
+                        },
+                    },
+                },
+            },
+        }),
     ],
     head: [
         ["link", {rel: "preconnect", href: "https://fonts.googleapis.com"}],
