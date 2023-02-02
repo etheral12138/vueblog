866be44f68e1369552966099f49bce7adf807e5a
etheral12138 <1694815257@qq.com>
2023-02-03 02:28:31 +0800

    修改于2-1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -25,11 +25,54 @@ export default defineUserConfig({
             },
         ),
         docsearchPlugin({
-            apiKey: "d9bd0d6112b4d1f34d32bc7f58fb9c3b",
+            apiKey: "7cfd751d4c7b5fe638591fc73c174a7d",
             appId: "4HTLIER5EQ",
             indexName: "etheral",
-            // disableUserPersonalization: true,
+            disableUserPersonalization: true,
             placeholder: "搜索文章",
+            locales: {
+                "/": {
+                    placeholder: "搜索文档",
+                    translations: {
+                        button: {
+                            buttonText: "搜索文档",
+                            buttonAriaLabel: "搜索文档",
+                        },
+                        modal: {
+                            searchBox: {
+                                resetButtonTitle: "清除查询条件",
+                                resetButtonAriaLabel: "清除查询条件",
+                                cancelButtonText: "取消",
+                                cancelButtonAriaLabel: "取消",
+                            },
+                            startScreen: {
+                                recentSearchesTitle: "搜索历史",
+                                noRecentSearchesText: "没有搜索历史",
+                                saveRecentSearchButtonTitle: "保存至搜索历史",
+                                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                                favoriteSearchesTitle: "收藏",
+                                removeFavoriteSearchButtonTitle: "从收藏中移除",
+                            },
+                            errorScreen: {
+                                titleText: "无法获取结果",
+                                helpText: "你可能需要检查你的网络连接",
+                            },
+                            footer: {
+                                selectText: "选择",
+                                navigateText: "切换",
+                                closeText: "关闭",
+                                searchByText: "搜索提供者",
+                            },
+                            noResultsScreen: {
+                                noResultsText: "无法找到相关结果",
+                                suggestedQueryText: "你可以尝试查询",
+                                openIssueText: "你认为该查询应该有结果？",
+                                openIssueLinkText: "点击反馈",
+                            },
+                        },
+                    },
+                },
+            },
         }),
     ],
     head: [
